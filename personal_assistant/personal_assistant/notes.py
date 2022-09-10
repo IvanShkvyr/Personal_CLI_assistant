21fbe5684fa37031ca20833367deca5041e123af
IvanShkvyr <Gis2011I@gmail.com>
2022-09-10 14:59:44 +0300

    test1

--- personal_assistant/personal_assistant/notes.py
+++ personal_assistant/personal_assistant/notes.py
@@ -40,38 +40,46 @@ class Notes(UserDict):      # Класс записок. Тип - словарь
     def __init__(self):
         self.data = {}
         self.note_id = 0
+        
     def add_note(self, note: Note):     # Добавляет новую записку в словарь
         self.data.update({self.note_id:note})
         self.note_id += 1
+
     def change_note_name(self, note_id: int, new_note_name):     # Изменяет название записки
         note = self.find_note_by_id(note_id)
         note.change_name(new_note_name)
         self.data[note_id] = note
+
     def change_note(self, note_id:int, new_note):   # Изменяет содержание записки
         note = self.find_note_by_id(note_id)
         note.change_note(new_note)
         self.data[note_id] = note
+
     def delete_note(self, note_id: int):    # Удаляет записку
         try:
             self.data.pop(note_id)
             self._id_order()
         except KeyError:
             print("This note does not exists!")
+
     def show_all(self):     # Показывает все записки
         for note_id, note in self.data.items():
             print(f'Note ID: {note_id}')
             print(note)
+
     def find_note_by_id(self, note_id: int):    # Поиск записки по уникальному ID
         try:
             return self.data[note_id]
         except KeyError:
             print("This note does not exists!")
+
     def _id_order(self):    # Скрытый метод, вызывается при удалении записи. Упорядочивает уникальные ID записей во избежание наложения
         data = self.data.copy()
         self.data.clear()
         self.note_id = 0
         for note in data.values():
             self.add_note(note)
+
     def find_by_tags(self, *tags):      # Поиск по тэгам
         to_return = []
         tags = list(tags)
