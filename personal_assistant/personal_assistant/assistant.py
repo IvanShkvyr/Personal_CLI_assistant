ad6105673490a867c5bfd7202e32687c9fb5cbe7
Musfer <adzhymambetov@gmail.com>
2022-09-10 14:25:40 +0300

    tasks 1-5
    initial commit

---
+++ personal_assistant/personal_assistant/assistant.py
@@ -0,0 +1,23 @@
+import classes
+from commands import commands, def_mod
+import sys
+from InquirerPy import prompt
+
+
+def main():
+    print("Welcome to your personal Python assistant!")
+    print("What can I do for you today?")
+    book = classes.AddressBook()
+    book.read_from_file()
+    while True:
+        command = input()
+        mode, data = def_mod(command)
+        output = commands.get(mode)(book, data)
+        print(output)
+        if output == "Good bye!":
+            book.write_to_file()
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
